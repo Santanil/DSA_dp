0ab54ccc183b50ea710fdeeac6517852034c5996
SANTANIL BASAK <santanilbasak@gmail.com>
2022-09-14 21:44:29 +0530

    Update Frog Jump.java

--- Frog Jump.java
+++ Frog Jump.java
@@ -2,6 +2,12 @@ https://www.codingninjas.com/codestudio/problems/frog-jump_3621012?source=youtub
 
 https://www.youtube.com/watch?v=EgG3jsGoPvQ&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=4
 
+There is a frog on the 1st step of an N stairs long staircase. The frog wants to reach the Nth stair. 
+HEIGHT[i] is the height of the (i+1)th stair.If Frog jumps from ith to jth stair, the energy lost in the jump is given by |HEIGHT[i-1] - HEIGHT[j-1] |.
+In the Frog is on ith staircase, he can jump either to (i+1)th stair or to (i+2)th stair. Your task is to find the minimum total energy used by the frog 
+to reach from 1st stair to Nth stair.
+
+
 //BF
 //TC:O(2^N)
 //SC:O(N)
